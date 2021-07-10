71c1518a0c5bdd370451f22de379dd98234e740c
Your Name <you@example.com>
2021-07-09 22:37:48 -0700

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Addtask =({addTask})=>{
+  const [value,updateValue]=useState("");
+
+const handleSubmit=e=>{
+  e.preventDefault();
+  if(value!=="")
+  {
+    addTask(value)
+    updateValue("");
+  }
+}
+
+return(
+
+  <form onSubmit={handleSubmit}>
+    <input
+    type="text"
+    value={value}
+    placeholder="enter your task to do"
+    onChange= {e =>updateValue(e.target.value)}
+    />
+    <button type="submit"><i class="material-icons">add</i></button>
+  </form>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+const ToDoList= () => {
+  
+  const mddTask=text=>updateTask([...tasks,{text}])
+  const[tasks,updateTask] = useState([
+    {
+    text:"Wake Up",
+    isCompleted:false
+    },
+    {
+      text:"FreshUp",
+      isCompleted:false
+    },
+    {
+      text:"Man",
+      isCompleted:false
+    },
+  ]
+  );
+
+  const toggleTask = index=>{
+    const newTask =[...tasks];
+    if(newTask[index].isCompleted)
+    {
+      newTask[index].isCompleted = false;
+    
+    }else{
+    newTask[index].isCompleted = true;
+    }
+    updateTask(newTask)
+  }
+  const removeTask= index=>{
+    const newTask=[...tasks];
+    newTask.splice(index,1)
+    updateTask(newTask);
+  }
+  return(
+
+    <div className="List-of-Task-to-do">
+    <Addtask addTask={mddTask}/>
+      {tasks.map((task,index)=>(
+        <div className="task-status">
+          <span onClick ={() => toggleTask(index)} className={task.isCompleted? "completed-task":"task-name"}>
+          {index} 
+          {task.text}
+          </span>
+          <button onClick={()=>removeTask(index)}> <i className="material-icons">delete</i></button>
+          
+         </div>
+      ))}
+      
+    </div>
+  );
+  }
+
+ReactDOM.render(<ToDoList/>,document.getElementById('root'));
